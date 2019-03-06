70f993de1c4b3dfd03f05485f38a15cf5db2d2cc
Louis <louis.foged@gmail.com>
2019-03-06 23:23:55 +0100

    adds initial draft of 'projects' section

--- js/app.js
+++ js/app.js
@@ -3,14 +3,17 @@
 // select all <a> tags with class 'to-scroll' 
 const links = document.querySelectorAll('a.to-scroll');
 
-// attach event listener to each link (above) => pass event & href (link.hash)
+// attach event listener to each link (above) & pass event & href value
+// ASIDE: 'link.hash' = alternative syntax for 'link.getAttribute('href')'
 links.forEach(link => {
-  link.addEventListener('click', (event) => smoothScroll(event, link.hash));
+  link.addEventListener('click', (event) => {
+    return smoothScroll(event, link.getAttribute('href'));
+  });
 });
 
 // scroll an element into view smoothly
 const smoothScroll = (event, targetEl) => {
   event.preventDefault();
-  const toScroll = document.querySelector(targetEl);
-  return toScroll.scrollIntoView({ behavior: "smooth", block: "start" });
+  const scrollTarget = document.querySelector(targetEl);
+  scrollTarget.scrollIntoView({ behavior: "smooth", block: "start" });
 };
