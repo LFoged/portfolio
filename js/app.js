cc74b7fede5413ffaa7c8ce5f79c2f1fa5666de1
Louis <louis.foged@gmail.com>
2019-03-10 19:19:23 +0100

    adds JS for nav-link display of current section

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/* SMOOTH SCROLL */
 // select all <a> tags with class 'to-scroll' 
 const links = document.querySelectorAll('a.to-scroll');
 
@@ -16,4 +17,61 @@ const smoothScroll = (event, targetEl) => {
   event.preventDefault();
   const scrollTarget = document.querySelector(targetEl);
   scrollTarget.scrollIntoView({ behavior: "smooth", block: "start" });
+
 };
+
+
+/* NAV-LINK - APLLY CURRENT SECTION */
+// apply 'current' class to correct nav-link on page load
+const isInViewPort = (() => {
+  const doc = document;
+
+  const navLinks = doc.querySelectorAll('.nav-link');
+
+  // sections
+  const homeSection = doc.querySelector('#home');
+  const projectsSection = doc.querySelector('#projects');
+  const aboutSection = doc.querySelector('#about');
+
+  // home section top & bottom
+  const homeRect = homeSection.getBoundingClientRect();
+  const homeTop = homeRect.top;
+  const homeBtm = homeRect.bottom;
+
+  // projects section top & bottom
+  const projectsRect = projectsSection.getBoundingClientRect();
+  const projectsTop = projectsRect.top;
+  const projectsBtm = projectsRect.bottom;
+
+  // about section top & bottom
+  const aboutRect = aboutSection.getBoundingClientRect();
+  const aboutTop = aboutRect.top;
+  const aboutBtm = aboutRect.bottom;
+
+  return (
+    (homeTop >= 0) && (homeBtm <= window.innerHeight)
+      ? navLinks[0].classList.add('current')
+      : (projectsTop >= 0) && (projectsBtm <= window.innerHeight)
+        ? navLinks[1].classList.add('current')
+        : (aboutTop >= 0) && (aboutBtm <= window.innerHeight)
+          ? navLinks[2].classList.add('current')
+          : false
+  );
+})();
+
+
+// apply 'current' class to correct nav-link on click
+document.querySelector('.nav-links').addEventListener('click', (event) => {
+  const doc = document;
+  const navLinks = doc.querySelectorAll('.nav-link');
+
+  // remove 'current' class from all nav-links
+  navLinks.forEach((link) => link.classList.remove('current'));
+
+  // if user clicks on an icon, add 'current' class to parent nav-link
+  if (event.target['tagName'] === 'IMG') {
+    return event.target['parentElement'].classList.add('current');
+  }
+
+  return event.target['classList'].add('current');
+});
